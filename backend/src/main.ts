6866b6d3ab5d6c8b39cf11a9e348125cf2129eac
csaszi <csaszika4@gmail.com>
2019-02-09 18:57:47 +0100

    initialize swagger only in dev mode

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,14 +6,16 @@ import { AppModule } from './app.module';
 async function bootstrap(): Promise<any> {
   const app = await NestFactory.create(AppModule);
 
-  const options = new DocumentBuilder()
+  if (AppModule.isDev) {
+    const options = new DocumentBuilder()
         .setTitle('Dogs example')
         .setDescription('The dogs API description')
         .setVersion('1.0')
         .addTag('dogs')
         .build();
-  const document = SwaggerModule.createDocument(app, options);
-  SwaggerModule.setup('api', app, document);
+    const document = SwaggerModule.createDocument(app, options);
+    SwaggerModule.setup('api', app, document);
+  }
 
   await app.listen(AppModule.port);
 }
