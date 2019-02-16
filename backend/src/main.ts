2c93b7989bc1f45cfe23fe26c610f778d1cd8bc7
Balint Csaszar <balint.csaszar@otpbank.hu>
2019-02-16 16:24:54 +0100

    update swagger doc

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,16 +5,27 @@ import { AppModule } from './app.module';
 // tslint:disable-next-line:only-arrow-functions
 async function bootstrap(): Promise<any> {
   const app = await NestFactory.create(AppModule);
+  const hostDomain = AppModule.isDev ? `${AppModule.host}:${AppModule.port}` : AppModule.host;
 
   if (AppModule.isDev) {
-    const options = new DocumentBuilder()
-        .setTitle('Swagger docs')
-        .setDescription('APIs descriptions')
-        .setVersion('1.0')
-        .addTag('apis')
-        .build();
-    const document = SwaggerModule.createDocument(app, options);
-    SwaggerModule.setup('api', app, document);
+      const options = new DocumentBuilder()
+          .setTitle('Swagger docs')
+          .setDescription('APIs descriptions')
+          .setVersion('1.0')
+          .addTag('apis')
+          .build();
+
+      const document = SwaggerModule.createDocument(app, options);
+
+      SwaggerModule.setup('api', app, document, {
+          swaggerUrl: `${hostDomain}/api/docs-json`,
+          explorer: true,
+          swaggerOptions: {
+              docExpansion: 'list',
+              filter: true,
+              showRequestDuration: true,
+          },
+      });
   }
 
   await app.listen(AppModule.port);
